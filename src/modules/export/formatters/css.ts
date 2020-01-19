cb9baceae945946ebf2c18b7ca8617812132f947
Dmitrii Bykov <sukazavr@gmail.com>
2020-01-19 08:33:31 +0700

    fix(export): convert $nest to CSS rules

--- src/modules/export/formatters/css.ts
+++ src/modules/export/formatters/css.ts
@@ -1,14 +1,24 @@
+import { NestedCSSProperties } from 'typestyle/lib/types';
+
 import {
     CANVAS_ID, ECanvasType, EElementType, IElements, TElementGenericOrBody,
 } from '../../../generic/states/elements';
 import { getNormalizedElementCSSProperties } from '../../../generic/style-helpers/normalize';
-import { isElementGeneric } from '../../../generic/supply/type-guards';
+import { isDefined, isElementGeneric } from '../../../generic/supply/type-guards';
 import {
     addIndent, asHl1, asHl2, asHl3, asLine, classNameKeyToString, getUniqElementClassName,
     selectElementWithFallback,
 } from './utils';
 
-const createClass = (elementID: string, elements: IElements) => {
+const createClass = (className: string, rules: NestedCSSProperties) => [
+  `.${asHl1(className)} ${asHl2('{')}`,
+  ...Object.entries(rules).map(([name, rule]) =>
+    addIndent(`${classNameKeyToString(name)}: ${asHl3(rule)};`)
+  ),
+  asHl2('}'),
+];
+
+const targetToLines = (elementID: string, elements: IElements) => {
   const className = getUniqElementClassName(elementID, elements);
   const element = selectElementWithFallback(elementID, elements);
   const parentType = isElementGeneric(element)
@@ -21,18 +31,29 @@ const createClass = (elementID: string, elements: IElements) => {
     parentType,
     excludeComponentStyle: true,
   });
-  return [
+  const $nest = rules.$nest;
+  delete rules.$nest;
+  const lines = [
     `.${asHl1(className)} ${asHl2('{')}`,
     ...Object.entries(rules).map(([name, rule]) =>
       addIndent(`${classNameKeyToString(name)}: ${asHl3(rule)};`)
     ),
     asHl2('}'),
   ];
+  if (isDefined($nest)) {
+    Object.entries($nest).forEach(([subClassName, subRules]) => {
+      lines.push('');
+      lines.push(
+        ...createClass(subClassName.replace('&', className), subRules as NestedCSSProperties)
+      );
+    });
+  }
+  return lines;
 };
 
 const makeTree = (targetID: string, elements: IElements): string[] => {
   const target = selectElementWithFallback(targetID, elements);
-  const lines = createClass(targetID, elements);
+  const lines = targetToLines(targetID, elements);
   if (target.type !== EElementType.Component) {
     lines.push(
       ...target.children.reduce<string[]>(
